231f43e3755c2d86ee4b31ca6b88783678106abd
Aleks153 <lermontov.v.pyatigorske@yandex.ru>
2024-05-25 22:13:14 +0500

    Add files via upload

---
+++ Bazovye struktury dannyh.py
@@ -0,0 +1,37 @@
+print('Решение задачи "Длина слова"')
+a = 'Длина слова'
+print(len(a))
+print('__________')
+
+print('Решение задачи "Суммы и разности"')
+first = 123
+second = 456
+summa = first + second
+diff = first - second
+print(summa,diff)
+print('__________')
+
+print('Решение задачи "Среднее арифметическое"')
+first = 456
+second = 789
+third = 123
+summ = first + second + third
+mean = summ / 3
+print(int(mean))
+print('__________')
+
+print('Решение задачи "Простые строчки"')
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string+', '+first_string)
+print('или')
+print(second_string+',',first_string)
+print('___________')
+
+print('Решение задачи "Сложная формула"')
+a = 123
+b = 456
+c = 789
+f = ((a*b)+(a*c))**3/2
+print(f)
+
